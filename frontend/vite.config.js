bff08453ae08129763a634e017b033253bea7d7f
tamil <tamilarasisanjai23@gmail.com>
2026-09-19 18:12:56 +0530

    Configure multi-page Vite build

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'index.html'),
+        login: resolve(__dirname, 'login.html'),
+        register: resolve(__dirname, 'register.html'),
+        adminDashboard: resolve(
+          __dirname,
+          'admin-dashboard.html'
+        ),
+        volunteerDashboard: resolve(
+          __dirname,
+          'volunteer-dashboard.html'
+        ),
+        radio: resolve(
+          __dirname,
+          'radio.html'
+        ),
+      },
+    },
+  },
 })
